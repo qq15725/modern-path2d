013354d167f6327c8ba92dab344e9850f133a480
wengxiangmin <157215725@qq.com>
2024-10-14 11:21:23 +0800

    feat: parseSvg supports base64 dataURI and utf8 dataURI

--- docs/main.ts
+++ docs/main.ts
@@ -41,9 +41,7 @@ async function main(): Promise<void> {
   ctx2.stroke(path2)
   console.warn(path1)
 
-  const paths = parseSvg(`<svg xmlns="http://www.w3.org/2000/svg" width="72" height="72" viewBox="0 0 72 72" fill="none">
-<path d="M19.9277 47.3589H53.0078" stroke="#71E35B" stroke-width="5" stroke-linecap="round" stroke-dasharray="0.1 8"/>
-</svg>`)
+  const paths = parseSvg(`data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iNzIiIGhlaWdodD0iNzIiIHZpZXdCb3g9IjAgMCA3MiA3MiIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZD0iTTUxLjM2NDYgNDUuODY0MkM0OS43ODA4IDQ2LjI3ODIgNDcuOTA2IDQ2LjcwNSA0NS44NTg4IDQ3LjA4NTdNNDUuODU4OCA0Ny4wODU3QzM0LjE2NDkgNDkuMjYwNyAxNi44NDg2IDQ5LjkzNDMgMTYuMDI3NyAzOC4xNDg0QzE1LjIyIDI2LjU1MzMgMzIuMjY0IDIyLjM2MzYgNDUuNjEzNSAyNC41NjI2QzUzLjYwMSAyNS44NzgzIDU3LjQ1MDcgMjkuNjIwOCA1Ny45Mjg1IDM0LjIzN0M1OC4yODExIDM3LjY0MzUgNTUuNzc4IDQzLjM3MDIgNDUuODU4OCA0Ny4wODU3Wk00NS44NTg4IDQ3LjA4NTdDNDIuMzM2NyA0OC40MDUxIDM3Ljg3OTUgNDkuNDcwOCAzMi4yODMgNTAuMDg5MSIgc3Ryb2tlPSIjRkZDMzAwIiBzdHJva2Utd2lkdGg9IjIuNSIgc3Ryb2tlLWxpbmVjYXA9InJvdW5kIi8+Cjwvc3ZnPgo=`)
   console.warn(paths)
   paths.forEach(path => path1.addPath(path))
 
--- src/svg-dom/parseSvg.ts
+++ src/svg-dom/parseSvg.ts
@@ -1,10 +1,26 @@
 import type { Path2D } from '../paths'
 import { parseNode } from './parseNode'
 
+const dataUri = 'data:image/svg+xml;'
+const base64DataUri = `${dataUri}base64,`
+const utf8DataUri = `${dataUri}charset=utf8,`
+
 export function parseSvg(svg: string | SVGElement): Path2D[] {
   let node
+  let xml
   if (typeof svg === 'string') {
-    node = new DOMParser().parseFromString(svg, 'image/svg+xml').documentElement as unknown as SVGElement
+    if (svg.startsWith(base64DataUri)) {
+      svg = svg.substring(base64DataUri.length, svg.length)
+      xml = atob(svg)
+    }
+    else if (svg.startsWith(utf8DataUri)) {
+      svg = svg.substring(utf8DataUri.length, svg.length)
+      xml = decodeURIComponent(svg)
+    }
+    else {
+      xml = svg
+    }
+    node = new DOMParser().parseFromString(xml, 'image/svg+xml').documentElement as unknown as SVGElement
   }
   else {
     node = svg
