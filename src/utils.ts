a4b142037a768f89b44380748960724d0faad170
wengxiangmin <157215725@qq.com>
2024-11-04 14:10:47 +0800

    feat: getPathsBoundingBox

---
+++ src/utils.ts
@@ -0,0 +1,12 @@
+import type { Path2D } from './paths'
+import { BoundingBox, Vector2 } from './math'
+
+export function getPathsBoundingBox(paths: Path2D[]): BoundingBox | undefined {
+  if (!paths.length) {
+    return undefined
+  }
+  const min = Vector2.MAX
+  const max = Vector2.MIN
+  paths.forEach(path => path.getMinMax(min, max))
+  return new BoundingBox(min.x, min.y, max.x - min.x, max.y - min.y)
+}
