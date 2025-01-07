c837b3d7465519ce84ee83f4414fe98085e37fc7
wxm <157215725@qq.com>
2025-01-07 17:30:37 +0800

    feat: rename `svgToDom` to `svgToDOM`

--- src/svg/dom/svgToDOM.ts
+++ src/svg/dom/svgToDOM.ts
@@ -1,7 +1,7 @@
 const dataUri = 'data:image/svg+xml;'
 const base64DataUri = `${dataUri}base64,`
 const utf8DataUri = `${dataUri}charset=utf8,`
-export function svgToDom(svg: string | SVGElement): SVGElement {
+export function svgToDOM(svg: string | SVGElement): SVGElement {
   if (typeof svg === 'string') {
     let xml
     if (svg.startsWith(base64DataUri)) {
--- src/svg/dom/svgToPath2DSet.ts
+++ src/svg/dom/svgToPath2DSet.ts
@@ -1,8 +1,8 @@
 import type { Path2DStyle } from '../../core'
 import { Path2DSet } from '../../core'
 import { parseNode } from './parseNode'
-import { svgToDom } from './svgToDom'
+import { svgToDOM } from './svgToDOM'
 
 export function svgToPath2DSet(svg: string | SVGElement): Path2DSet {
-  return new Path2DSet(parseNode(svgToDom(svg), {} as Partial<Path2DStyle>))
+  return new Path2DSet(parseNode(svgToDOM(svg), {} as Partial<Path2DStyle>))
 }
